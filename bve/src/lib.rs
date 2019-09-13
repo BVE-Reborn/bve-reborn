febc7e096acf3843b03cd69af05293734f7cc26f
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-09-13 15:20:29 -0400

    Touch up dependencies

--- bve/src/lib.rs
+++ bve/src/lib.rs
@@ -1,7 +1,1 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! Implementation of the high performance runtime logic for the game BVE-Reborn.
