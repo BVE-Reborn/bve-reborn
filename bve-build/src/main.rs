0d0639e74cd639e48ff9badebcb23a28975b805b
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-09-13 15:19:45 -0400

    Add coloring to build output

--- bve-build/src/main.rs
+++ bve-build/src/main.rs
@@ -1,6 +1,18 @@
 use std::process::Command;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Options {
+    #[structopt(short, long)]
+    color: Option<String>,
+}
 
 fn main() {
-    let mut child = Command::new("cargo").args(&["build", "--release"]).spawn().unwrap();
+    let parsed: Options = Options::from_args();
+
+    let mut args = vec![String::from("build"), String::from("--release")];
+    parsed.color.iter().for_each(|s| args.push(format!("--color={}", s)));
+
+    let mut child = Command::new("cargo").args(&args).spawn().unwrap();
     assert!(child.wait().unwrap().success());
 }
