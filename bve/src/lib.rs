0a6e5281b8316429e4ee90a2378907a5cf1c4cb4
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-09-14 00:34:32 -0400

    Add nightly features and warnings

--- bve/src/lib.rs
+++ bve/src/lib.rs
@@ -1,4 +1,17 @@
 //! Implementation of the high performance runtime logic for the game BVE-Reborn.
+
+#![feature(async_closure)]
+#![feature(test)]
+#![feature(type_alias_impl_trait)]
+
+#![allow(clippy::cognitive_complexity)]
+#![allow(clippy::float_cmp)]
+#![warn(unused)]
+#![deny(nonstandard_style)]
+#![deny(future_incompatible)]
+#![deny(rust_2018_idioms)]
+#![forbid(unsafe_code)]
+
 pub use datatypes::*;
 
 mod datatypes;
