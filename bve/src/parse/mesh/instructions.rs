117a440b91c180ad939f23d98a7a197385a7fea8
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-09-22 19:19:20 -0400

    Finish b3d/csv deserialization?

--- bve/src/parse/mesh/instructions.rs
+++ bve/src/parse/mesh/instructions.rs
@@ -261,7 +261,98 @@ fn deserialize_instruction(
             let parsed: AddVertex = record.deserialize(None)?;
             InstructionData::AddVertex(parsed)
         }
-        _ => unimplemented!(),
+        InstructionType::AddFace => {
+            let mut parsed: AddFace = record.deserialize(None)?;
+            parsed.sides = Sides::One;
+            InstructionData::AddFace(parsed)
+        }
+        InstructionType::AddFace2 => {
+            let mut parsed: AddFace = record.deserialize(None)?;
+            parsed.sides = Sides::Two;
+            InstructionData::AddFace(parsed)
+        }
+        InstructionType::Cube => {
+            let parsed: Cube = record.deserialize(None)?;
+            InstructionData::Cube(parsed)
+        }
+        InstructionType::Cylinder => {
+            let parsed: Cylinder = record.deserialize(None)?;
+            InstructionData::Cylinder(parsed)
+        }
+        InstructionType::Translate => {
+            let mut parsed: Translate = record.deserialize(None)?;
+            parsed.application = ApplyTo::SingleMesh;
+            InstructionData::Translate(parsed)
+        }
+        InstructionType::TranslateAll => {
+            let mut parsed: Translate = record.deserialize(None)?;
+            parsed.application = ApplyTo::AllMeshes;
+            InstructionData::Translate(parsed)
+        }
+        InstructionType::Scale => {
+            let mut parsed: Scale = record.deserialize(None)?;
+            parsed.application = ApplyTo::SingleMesh;
+            InstructionData::Scale(parsed)
+        }
+        InstructionType::ScaleAll => {
+            let mut parsed: Scale = record.deserialize(None)?;
+            parsed.application = ApplyTo::AllMeshes;
+            InstructionData::Scale(parsed)
+        }
+        InstructionType::Rotate => {
+            let mut parsed: Rotate = record.deserialize(None)?;
+            parsed.application = ApplyTo::SingleMesh;
+            InstructionData::Rotate(parsed)
+        }
+        InstructionType::RotateAll => {
+            let mut parsed: Rotate = record.deserialize(None)?;
+            parsed.application = ApplyTo::AllMeshes;
+            InstructionData::Rotate(parsed)
+        }
+        InstructionType::Sheer => {
+            let mut parsed: Sheer = record.deserialize(None)?;
+            parsed.application = ApplyTo::SingleMesh;
+            InstructionData::Sheer(parsed)
+        }
+        InstructionType::SheerAll => {
+            let mut parsed: Sheer = record.deserialize(None)?;
+            parsed.application = ApplyTo::AllMeshes;
+            InstructionData::Sheer(parsed)
+        }
+        InstructionType::Mirror => {
+            let mut parsed: Mirror = record.deserialize(None)?;
+            parsed.application = ApplyTo::SingleMesh;
+            InstructionData::Mirror(parsed)
+        }
+        InstructionType::MirrorAll => {
+            let mut parsed: Mirror = record.deserialize(None)?;
+            parsed.application = ApplyTo::AllMeshes;
+            InstructionData::Mirror(parsed)
+        }
+        InstructionType::SetColor => {
+            let parsed: SetColor = record.deserialize(None)?;
+            InstructionData::SetColor(parsed)
+        }
+        InstructionType::SetEmissiveColor => {
+            let parsed: SetEmissiveColor = record.deserialize(None)?;
+            InstructionData::SetEmissiveColor(parsed)
+        }
+        InstructionType::SetBlendMode => {
+            let parsed: SetBlendMode = record.deserialize(None)?;
+            InstructionData::SetBlendMode(parsed)
+        }
+        InstructionType::LoadTexture => {
+            let parsed: LoadTexture = record.deserialize(None)?;
+            InstructionData::LoadTexture(parsed)
+        }
+        InstructionType::SetDecalTransparentColor => {
+            let parsed: SetDecalTransparentColor = record.deserialize(None)?;
+            InstructionData::SetDecalTransparentColor(parsed)
+        }
+        InstructionType::SetTextureCoordinates => {
+            let parsed: SetTextureCoordinates = record.deserialize(None)?;
+            InstructionData::SetTextureCoordinates(parsed)
+        }
     };
     Ok(Instruction { data, span })
 }
@@ -317,7 +408,7 @@ pub fn create_instructions(input: String, file_type: FileType) -> InstructionLis
         }
     }
 
-    unimplemented!()
+    instructions
 }
 
 #[cfg(test)]
