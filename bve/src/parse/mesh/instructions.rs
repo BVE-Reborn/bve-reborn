ee73cafc04449ace56418d4868abcea8c78a6f20
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-09-23 07:51:12 -0400

    [parse::mesh] Fix compile errors

--- bve/src/parse/mesh/instructions.rs
+++ bve/src/parse/mesh/instructions.rs
@@ -436,29 +436,29 @@ mod test {
 
         #[test]
         fn comma_add() {
-            assert_eq!(b3d_to_csv_syntax("myinstruction arg1".into()), "myinstruction, arg1\n");
+            assert_eq!(b3d_to_csv_syntax("myinstruction arg1"), "myinstruction, arg1\n");
         }
         #[test]
         fn multiline_comma_add() {
             assert_eq!(
-                b3d_to_csv_syntax("myinstruction arg1\nmyother arg2".into()),
+                b3d_to_csv_syntax("myinstruction arg1\nmyother arg2"),
                 "myinstruction, arg1\nmyother, arg2\n"
             );
         }
 
         #[test]
         fn spaceless() {
-            assert_eq!(b3d_to_csv_syntax("myinstruction".into()), "myinstruction\n");
-            assert_eq!(b3d_to_csv_syntax("".into()), "\n");
+            assert_eq!(b3d_to_csv_syntax("myinstruction"), "myinstruction\n");
+            assert_eq!(b3d_to_csv_syntax(""), "\n");
         }
 
         #[test]
         fn multiline_spaceless() {
             assert_eq!(
-                b3d_to_csv_syntax("myinstruction\n\nfk2".into()),
+                b3d_to_csv_syntax("myinstruction\n\nfk2"),
                 "myinstruction\n\nfk2\n"
             );
-            assert_eq!(b3d_to_csv_syntax("\n\n".into()), "\n\n");
+            assert_eq!(b3d_to_csv_syntax("\n\n"), "\n\n");
         }
     }
 
@@ -469,23 +469,23 @@ mod test {
 
         macro_rules! single_line_instruction_assert {
             ( $inputB3D:literal, $inputCSV:literal, $args:literal, $data:expr ) => {
-                let result_a = create_instructions(concat!($inputB3D, " ", $args).into(), FileType::B3D);
+                let result_a = create(concat!($inputB3D, " ", $args).into(), FileType::B3D);
                 if !result_a.errors.is_empty() {
                     panic!("ERRORS!! {:#?}", result_a)
                 }
                 assert_eq!(
-                    result_a.instructions[0],
+                    *result_a.instructions.get(0).unwrap(),
                     Instruction {
                         data: $data,
                         span: Span { line: Some(1) }
                     }
                 );
-                let result_b = create_instructions(concat!($inputCSV, ",", $args).into(), FileType::CSV);
+                let result_b = create(concat!($inputCSV, ",", $args).into(), FileType::CSV);
                 if !result_b.errors.is_empty() {
                     panic!("ERRORS!! {:#?}", result_b)
                 }
                 assert_eq!(
-                    result_b.instructions[0],
+                    *result_a.instructions.get(0).unwrap(),
                     Instruction {
                         data: $data,
                         span: Span { line: Some(1) }
