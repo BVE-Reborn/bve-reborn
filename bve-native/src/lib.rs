8ca095b7bce65a5e788c9dbfe887fc41be8a8a3c
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-09-23 01:24:01 -0400

    [cargo] Warning party

--- bve-native/src/lib.rs
+++ bve-native/src/lib.rs
@@ -1,1 +1,25 @@
 //! C API for BVE-Reborn high performance libraries.
+
+// Rust warnings
+#![allow(unsafe_code)] // We're in an ffi
+#![warn(unused)]
+#![deny(nonstandard_style)]
+#![deny(future_incompatible)]
+#![deny(rust_2018_idioms)]
+// Clippy warnings
+#![warn(clippy::cargo)]
+#![warn(clippy::nursery)]
+#![warn(clippy::pedantic)]
+#![allow(clippy::cognitive_complexity)] // This is dumb
+#![allow(clippy::multiple_crate_versions)] // Dependencies are hard
+// Clippy Restrictions
+#![warn(clippy::clone_on_ref_ptr)]
+#![warn(clippy::dbg_macro)]
+#![warn(clippy::get_unwrap)]
+#![warn(clippy::multiple_inherent_impl)]
+#![warn(clippy::option_unwrap_used)]
+#![warn(clippy::print_stdout)]
+#![warn(clippy::result_unwrap_used)]
+#![warn(clippy::unimplemented)]
+#![warn(clippy::wildcard_enum_match_arm)]
+#![warn(clippy::wrong_pub_self_convention)]
--- bve/src/lib.rs
+++ bve/src/lib.rs
@@ -1,15 +1,29 @@
 //! Implementation of the high performance runtime logic for the game BVE-Reborn.
 
-#![feature(async_closure)]
-#![feature(test)]
-#![feature(type_alias_impl_trait)]
-#![allow(clippy::cognitive_complexity)]
-#![allow(clippy::float_cmp)]
+// Rust warnings
 #![warn(unused)]
 #![deny(nonstandard_style)]
 #![deny(future_incompatible)]
 #![deny(rust_2018_idioms)]
 #![forbid(unsafe_code)]
+// Clippy warnings
+#![warn(clippy::cargo)]
+#![warn(clippy::nursery)]
+#![warn(clippy::pedantic)]
+#![allow(clippy::cognitive_complexity)] // This is dumb
+#![allow(clippy::multiple_crate_versions)] // Dependencies are hard
+// Clippy Restrictions
+#![warn(clippy::clone_on_ref_ptr)]
+#![warn(clippy::dbg_macro)]
+#![warn(clippy::get_unwrap)]
+#![warn(clippy::mem_forget)]
+#![warn(clippy::multiple_inherent_impl)]
+#![warn(clippy::option_unwrap_used)]
+#![warn(clippy::print_stdout)]
+#![warn(clippy::result_unwrap_used)]
+#![warn(clippy::unimplemented)]
+#![warn(clippy::wildcard_enum_match_arm)]
+#![warn(clippy::wrong_pub_self_convention)]
 
 pub use datatypes::*;
 
