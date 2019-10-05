9e7170bab0baa0215f9b354e922819d4b84f0f7d
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-10-04 23:44:53 -0400

    [parse::mesh] Add dispatch Executable impl

--- bve/src/parse/mesh/instructions/execution.rs
+++ bve/src/parse/mesh/instructions/execution.rs
@@ -1,9 +1,10 @@
+use crate::parse::mesh::instructions::{Instruction, InstructionData};
 use crate::parse::mesh::{BlendMode, FaceData, Glow, ParsedStaticObject, Vertex};
 use crate::{ColorU8RGB, ColorU8RGBA};
 use smallvec::SmallVec;
 
 trait Executable {
-    fn execute(&self, ctx: MeshBuildContext);
+    fn execute(&self, ctx: &mut MeshBuildContext);
 }
 
 struct MeshBuildContext {
@@ -25,4 +26,27 @@ struct ExpandedFaceData {
     blend_mode: BlendMode,
     glow: Glow,
     double_sided: bool,
+}
+
+impl Executable for Instruction {
+    fn execute(&self, ctx: &mut MeshBuildContext) {
+        match &self.data {
+            InstructionData::CreateMeshBuilder(data) => data.execute(),
+            InstructionData::AddVertex(data) => data.execute(),
+            InstructionData::AddFace(data) => data.execute(),
+            InstructionData::Cube(data) => data.execute(),
+            InstructionData::Cylinder(data) => data.execute(),
+            InstructionData::Translate(data) => data.execute(),
+            InstructionData::Scale(data) => data.execute(),
+            InstructionData::Rotate(data) => data.execute(),
+            InstructionData::Sheer(data) => data.execute(),
+            InstructionData::Mirror(data) => data.execute(),
+            InstructionData::SetColor(data) => data.execute(),
+            InstructionData::SetEmissiveColor(data) => data.execute(),
+            InstructionData::SetBlendMode(data) => data.execute(),
+            InstructionData::LoadTexture(data) => data.execute(),
+            InstructionData::SetDecalTransparentColor(data) => data.execute(),
+            InstructionData::SetTextureCoordinates(data) => data.execute(),
+        }
+    }
 }
