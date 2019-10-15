afba247e2bb16b5c750b8a39df0a93613c64e052
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-10-15 10:38:31 -0400

    [fuzz] Finish basic fuzzing infrastructure

--- bve-fuzz/src/bin/fuzz-parse-mesh.rs
+++ bve-fuzz/src/bin/fuzz-parse-mesh.rs
@@ -2,8 +2,8 @@ fn main() {
     loop {
         let closure = |data: &[u8]| {
             let string = String::from_utf8_lossy(data);
-            bve::parse::mesh::instructions::create_instructions(&string, bve::parse::mesh::FileType::B3D);
-            bve::parse::mesh::instructions::create_instructions(&string, bve::parse::mesh::FileType::CSV);
+            let _result = bve::parse::mesh::instructions::create_instructions(&string, bve::parse::mesh::FileType::B3D);
+            let _result = bve::parse::mesh::instructions::create_instructions(&string, bve::parse::mesh::FileType::CSV);
         };
         honggfuzz::fuzz!(|data: &[u8]| { closure(data) });
     }
