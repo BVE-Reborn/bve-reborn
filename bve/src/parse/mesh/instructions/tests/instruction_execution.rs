ec506353ddc4b68164229f2eba83818dcbf11963
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2019-12-08 01:28:09 -0500

    [parse::mesh] Add basic test asserts

--- bve/src/parse/mesh/instructions/tests/instruction_execution.rs
+++ bve/src/parse/mesh/instructions/tests/instruction_execution.rs
@@ -1,5 +1,6 @@
 use crate::parse::mesh::instructions::*;
 use crate::parse::mesh::Span;
+use cgmath::Array;
 use cgmath::Vector3;
 
 macro_rules! generate_instruction_list {
@@ -22,23 +23,30 @@ macro_rules! generate_instruction_list {
 fn single_mesh() {
     let v = generate_instruction_list!(
         0: AddVertex {
-            position: Vector3::new(0.0, 1.0, 1.0),
-            normal: Vector3::new(0.0, 1.0, 1.0),
+            position: Vector3::new(0.0, 0.0, 0.0),
+            normal: Vector3::from_value(0.0),
         },
         1: AddVertex {
-            position: Vector3::new(0.0, 2.0, 2.0),
-            normal: Vector3::new(0.0, 2.0, 2.0),
+            position: Vector3::new(-0.866025, 0.0, 0.5),
+            normal: Vector3::from_value(0.0),
         },
         2: AddVertex {
-            position: Vector3::new(0.0, 3.0, 3.0),
-            normal: Vector3::new(0.0, 3.0, 3.0),
+            position: Vector3::new(0.866025, 0.0, 0.5),
+            normal: Vector3::from_value(0.0),
         },
         3: AddFace {
             indexes: vec![0, 1, 2],
             sides: Sides::One,
+        },
+        4: LoadTexture {
+            daytime: String::from("day_tex"),
+            nighttime: String::from("night_tex"),
         }
     );
 
-    let _result = execution::generate_meshes(v);
-    dbg!(_result);
+    let result = execution::generate_meshes(v);
+    let first_mesh = &result.meshes[0];
+    assert_eq!(first_mesh.vertices[0].position, Vector3::new(0.0, 0.0, 0.0));
+    assert_eq!(first_mesh.vertices[1].position, Vector3::new(-0.866025, 0.0, 0.5));
+    assert_eq!(first_mesh.vertices[2].position, Vector3::new(0.866025, 0.0, 0.5));
 }
