a8dfca5cb1619cf181fbdb1c13911eb08b27694a
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-01-08 19:18:07 -0500

    [parse::mesh] Add texture coord test

--- bve/src/parse/mesh/instructions/tests/instruction_execution.rs
+++ bve/src/parse/mesh/instructions/tests/instruction_execution.rs
@@ -1,8 +1,8 @@
 use crate::parse::mesh::instructions::*;
 use crate::parse::mesh::{BlendMode, Glow, GlowAttenuationMode, Span};
 use crate::{ColorU8RGB, ColorU8RGBA};
-use cgmath::Array;
 use cgmath::Vector3;
+use cgmath::{Array, Vector2};
 
 macro_rules! generate_instruction_list {
     ($($num:literal: $id:ident { $($tokens:tt)* }),*) => {
@@ -54,6 +54,9 @@ fn single_mesh() {
     assert_eq!(mesh.vertices[2].position, Vector3::new(0.866025, 0.0, 0.5));
     for v in &mesh.vertices {
         assert_eq!(v.normal, Vector3::new(0.0, 1.0, 0.0));
+    }
+    for &v in &mesh.vertices {
+        assert_eq!(v.coord, Vector2::from_value(0.0));
     }
     assert_eq!(mesh.indices, vec![0, 1, 2]);
     assert_eq!(mesh.blend_mode, BlendMode::Normal);
@@ -144,6 +147,9 @@ fn double_mesh() {
     for v in &mesh.vertices {
         assert_eq!(v.normal, Vector3::new(0.0, 1.0, 0.0));
     }
+    for &v in &mesh.vertices {
+        assert_eq!(v.coord, Vector2::from_value(0.0));
+    }
     assert_eq!(mesh.indices, vec![0, 1, 2]);
     assert_eq!(mesh.blend_mode, BlendMode::Additive);
     assert_eq!(mesh.color, ColorU8RGBA::new(21, 22, 23, 24));
@@ -165,6 +171,9 @@ fn double_mesh() {
     for v in &mesh.vertices {
         assert_eq!(v.normal, Vector3::new(0.0, 1.0, 0.0));
     }
+    for &v in &mesh.vertices {
+        assert_eq!(v.coord, Vector2::from_value(0.0));
+    }
     assert_eq!(mesh.indices, vec![0, 1, 2]);
     assert_eq!(mesh.blend_mode, BlendMode::Normal);
     assert_eq!(mesh.color, ColorU8RGBA::from_value(255));
@@ -178,5 +187,70 @@ fn double_mesh() {
     assert_eq!(result.textures.len(), 2);
     assert_eq!(result.textures.lookup(0), Some("day_tex"));
     assert_eq!(result.textures.lookup(1), Some("other_day_tex"));
+    assert_eq!(result.errors.len(), 0);
+}
+
+#[test]
+fn texture_coords() {
+    let v = generate_instruction_list!(
+        0: CreateMeshBuilder {},
+        1: AddVertex {
+            position: Vector3::new(0.0, 0.0, 0.0),
+            normal: Vector3::from_value(0.0),
+        },
+        2: AddVertex {
+            position: Vector3::new(-0.866025, 0.0, 0.5),
+            normal: Vector3::from_value(0.0),
+        },
+        3: AddVertex {
+            position: Vector3::new(0.866025, 0.0, 0.5),
+            normal: Vector3::from_value(0.0),
+        },
+        4: AddFace {
+            indexes: vec![0, 1, 2],
+            sides: Sides::One,
+        },
+        5: LoadTexture {
+            daytime: String::from("day_tex"),
+            nighttime: String::from("night_tex"),
+        },
+        6: SetTextureCoordinates {
+            index: 0,
+            coords: Vector2::new(1.0, 1.0),
+        },
+        7: SetTextureCoordinates {
+            index: 1,
+            coords: Vector2::new(2.0, 2.0),
+        },
+        8: SetTextureCoordinates {
+            index: 2,
+            coords: Vector2::new(3.0, 3.0),
+        }
+    );
+
+    let result = execution::generate_meshes(v);
+    assert_eq!(result.meshes.len(), 1);
+    let mesh = &result.meshes[0];
+    assert_eq!(mesh.vertices[0].position, Vector3::new(0.0, 0.0, 0.0));
+    assert_eq!(mesh.vertices[1].position, Vector3::new(-0.866025, 0.0, 0.5));
+    assert_eq!(mesh.vertices[2].position, Vector3::new(0.866025, 0.0, 0.5));
+    for v in &mesh.vertices {
+        assert_eq!(v.normal, Vector3::new(0.0, 1.0, 0.0));
+    }
+    for (i, &v) in mesh.vertices.iter().enumerate() {
+        assert_eq!(v.coord, Vector2::from_value((i + 1) as f32));
+    }
+    assert_eq!(mesh.indices, vec![0, 1, 2]);
+    assert_eq!(mesh.blend_mode, BlendMode::Normal);
+    assert_eq!(mesh.color, ColorU8RGBA::from_value(255));
+    assert_eq!(
+        mesh.glow,
+        Glow {
+            attenuation_mode: GlowAttenuationMode::DivideExponent4,
+            half_distance: 0,
+        }
+    );
+    assert_eq!(result.textures.len(), 1);
+    assert_eq!(result.textures.lookup(0), Some("day_tex"));
     assert_eq!(result.errors.len(), 0);
 }
