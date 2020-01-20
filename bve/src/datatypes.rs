5f83c7447e22ac28e83c06b85742f9616f2541b2
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-01-20 00:10:28 -0500

    [docs] Add high level docs to datatypes and modules

--- bve/src/datatypes.rs
+++ bve/src/datatypes.rs
@@ -1,13 +1,20 @@
 use cgmath::{Vector1, Vector2, Vector3, Vector4};
 
-pub trait Asu32 {
+/// Reinterprets vector as a integer.
+///
+/// Used for sorting vectors where the actual order doesn't matter, but _an_ order needs to be made.
+pub(crate) trait Asu32 {
     #[must_use]
     fn as_u32(self) -> u32;
 }
 
+/// R color: Unsigned 8-bit integer per channel
 pub type ColorU8R = Vector1<u8>;
+/// RG color: Unsigned 8-bit integer per channel
 pub type ColorU8RG = Vector2<u8>;
+/// RGB color: Unsigned 8-bit integer per channel
 pub type ColorU8RGB = Vector3<u8>;
+/// RGBA color: Unsigned 8-bit integer per channel
 pub type ColorU8RGBA = Vector4<u8>;
 
 impl Asu32 for ColorU8R {
@@ -38,12 +45,20 @@ impl Asu32 for ColorU8RGBA {
     }
 }
 
+/// R color: Unsigned 16-bit integer per channel
 pub type ColorU16R = Vector1<u16>;
+/// RG color: Unsigned 16-bit integer per channel
 pub type ColorU16RG = Vector2<u16>;
+/// RGB color: Unsigned 16-bit integer per channel
 pub type ColorU16RGB = Vector3<u16>;
+/// RGBA color: Unsigned 16-bit integer per channel
 pub type ColorU16RGBA = Vector4<u16>;
 
+/// R color: 32-bit float per channel
 pub type ColorF32R = Vector1<f32>;
+/// RG color: 32-bit float per channel
 pub type ColorF32RG = Vector2<f32>;
+/// RGB color: 32-bit float per channel
 pub type ColorF32RGB = Vector3<f32>;
+/// RGBA color: 32-bit float per channel
 pub type ColorF32RGBA = Vector4<f32>;
