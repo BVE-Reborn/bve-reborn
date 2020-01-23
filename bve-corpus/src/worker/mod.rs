b9779c64ab168f9628e4ba179044d9850fd99d0f
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-01-23 00:28:04 -0500

    [corpus] Add a dummy load to be detected by timeout detection

--- bve-corpus/src/worker/mod.rs
+++ bve-corpus/src/worker/mod.rs
@@ -2,14 +2,16 @@ use crate::{File, FileKind, FileResult, SharedData};
 use crossbeam::atomic::AtomicCell;
 use crossbeam::channel::{Receiver, Sender};
 use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use std::path::PathBuf;
 use std::sync::atomic::Ordering;
-use std::sync::Arc;
+use std::sync::{Arc, Mutex};
 use std::thread::JoinHandle;
 use std::time::Instant;
 
 pub struct WorkerThread {
     pub handle: JoinHandle<()>,
     pub last_respond: Arc<AtomicCell<Instant>>,
+    pub last_file: Arc<Mutex<PathBuf>>,
 }
 
 pub fn create_worker_thread(
@@ -18,14 +20,20 @@ pub fn create_worker_thread(
     shared: &Arc<SharedData>,
 ) -> WorkerThread {
     let last_respond: Arc<AtomicCell<Instant>> = Arc::new(AtomicCell::new(Instant::now()));
+    let last_file = Arc::new(Mutex::new(PathBuf::new()));
     let handle = {
         let job_source = job_source.clone();
         let result_sink = result_sink.clone();
         let shared = Arc::clone(shared);
         let last_respond = Arc::clone(&last_respond);
-        std::thread::spawn(move || processing_loop(job_source, result_sink, shared, last_respond))
+        let last_file = Arc::clone(&last_file);
+        std::thread::spawn(move || processing_loop(job_source, result_sink, shared, last_respond, last_file))
     };
-    WorkerThread { handle, last_respond }
+    WorkerThread {
+        handle,
+        last_respond,
+        last_file,
+    }
 }
 
 fn processing_loop(
@@ -33,12 +41,17 @@ fn processing_loop(
     result_sink: Sender<FileResult>,
     shared: Arc<SharedData>,
     last_respond: Arc<AtomicCell<Instant>>,
+    last_file: Arc<Mutex<PathBuf>>,
 ) {
     while let Ok(file) = job_source.recv() {
-        std::thread::sleep(std::time::Duration::from_millis(1));
+        *last_file.lock().unwrap() = file.path.clone();
         let result = match file.kind {
+            FileKind::RouteRw => {
+                std::thread::sleep(std::time::Duration::from_secs(10));
+            }
             _ => {}
         };
+        std::thread::sleep(std::time::Duration::from_millis(1));
         shared.total.finished.fetch_add(1, Ordering::SeqCst);
     }
 }
