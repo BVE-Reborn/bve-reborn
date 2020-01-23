b7e8a7239c271a2fe811783917dc9a9e3f591667
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-01-22 19:48:35 -0500

    [corpus] Test thread kill functionality

--- bve-corpus/src/thread_kill.rs
+++ bve-corpus/src/thread_kill.rs
@@ -1,17 +1,37 @@
 use std::thread::JoinHandle;
 
 #[cfg(target_os = "windows")]
-pub fn kill_thread(thread: JoinHandle<()>) {
+pub fn kill_thread(thread: JoinHandle<()>) -> Option<()> {
     use std::os::windows::prelude::*;
 
     let handle: RawHandle = thread.into_raw_handle();
-    unsafe { winapi::um::processthreadsapi::TerminateThread(handle, 1) };
+    let res = unsafe { winapi::um::processthreadsapi::TerminateThread(handle, 1) };
+    if res == winapi::shared::minwindef::TRUE {
+        Some(())
+    } else {
+        None
+    }
 }
 
 #[cfg(target_family = "unix")]
-pub fn kill_thread(thread: JoinHandle<()>) {
+pub fn kill_thread(thread: JoinHandle<()>) -> Option<()> {
     use std::os::unix::thread::{JoinHandleExt, RawPthread};
 
     let handle: RawPthread = thread.into_pthread_t();
-    unsafe { libc::pthread_cancel(handle) };
+    let res = unsafe { libc::pthread_cancel(handle) };
+    if res == 0 { Some(()) } else { None }
+}
+
+#[cfg(test)]
+mod test {
+    use std::time::Duration;
+
+    #[test]
+    fn kill_thread() {
+        super::kill_thread(std::thread::spawn(|| {
+            std::thread::sleep(Duration::from_secs(1));
+            assert!(false, "Thread not killed in time.")
+        }))
+        .unwrap();
+    }
 }
