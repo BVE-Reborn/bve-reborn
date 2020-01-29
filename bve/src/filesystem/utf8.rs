af0bad7a4b17807cb7c542431621b17330b05a31
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-01-28 20:33:43 -0500

    [parse] Properly decode utf16 and utf8 with a bom

--- bve/src/filesystem/utf8.rs
+++ bve/src/filesystem/utf8.rs
@@ -11,16 +11,39 @@ use std::path::Path;
 pub fn read_convert_utf8(filename: impl AsRef<Path>) -> Result<String> {
     let bytes = read(filename)?;
 
-    let mut detector = EncodingDetector::new();
-    let ascii_only = !detector.feed(&bytes, true);
+    convert_to_utf8(bytes)
+}
 
-    if ascii_only {
-        Ok(String::from_utf8(bytes).expect("Only ascii characters detected, but utf8 validation failed"))
+fn convert_to_utf8(bytes: Vec<u8>) -> Result<String> {
+    // Byte order marks are not properly dealt with in chardetng, detect them here, encoding_rs will remove them
+    let encoding = if bytes.len() >= 2 && bytes[0..2] == [0xFF, 0xFE] {
+        encoding_rs::UTF_16LE
+    } else if bytes.len() >= 2 && bytes[0..2] == [0xFE, 0xFF] {
+        encoding_rs::UTF_16BE
+    } else if bytes.len() >= 3 && bytes[0..3] == [0xEF, 0xBB, 0xEF] {
+        encoding_rs::UTF_8
     } else {
-        let encoding = detector.guess(None, true);
+        let mut detector = EncodingDetector::new();
+        let ascii_only = !detector.feed(&bytes, true);
+        if ascii_only {
+            return Ok(String::from_utf8(bytes).expect("Only ascii characters detected, but utf8 validation failed"));
+        }
+        detector.guess(None, true)
+    };
 
-        let (result, ..) = encoding.decode(&bytes);
+    println!("{}", encoding.name());
 
-        Ok(result.to_string())
+    let (result, ..) = encoding.decode_with_bom_removal(&bytes);
+
+    Ok(result.to_string())
+}
+
+#[cfg(test)]
+mod test {
+    use super::convert_to_utf8;
+
+    #[test]
+    fn bom_removal() {
+        assert_eq!(convert_to_utf8(vec![0xFF, 0xFE]).unwrap(), "");
     }
 }
