9b21659ab12e0a004196e0d8b8c305ede9578c00
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-02-08 19:34:29 -0500

    [rex-sys] Attempt to fix rex, sdl2, and friends on windows

--- bve-rex-sys/build.rs
+++ bve-rex-sys/build.rs
@@ -5,63 +5,32 @@ use walkdir::{DirEntry, Error};
 trait BuildExt {
     /// Enable clang-cl only on windows
     fn enable_clang_cl(&mut self) -> &mut Self;
-    /// Platform specific defines
-    fn add_defines(&mut self) -> &mut Self;
-    /// Enable c11
-    fn enable_c11(&mut self) -> &mut Self;
-    /// Enable c++17
-    fn enable_cpp17(&mut self) -> &mut Self;
 }
 
-// impl BuildExt for cc::Build {
-//    fn enable_clang_cl(&mut self) -> &mut Self {
-//        cfg_if::cfg_if! {
-//            if #[cfg(target_os = "windows")] {
-//                let llvm_dir = std::env::var("LLVM_DIR").ok();
-//                let llvm_dir = llvm_dir.unwrap_or_else(|| String::from("C:/Program Files/LLVM/"));
-//                let mut clang_path = PathBuf::from(llvm_dir);
-//                clang_path.push("bin");
-//                clang_path.push("clang-cl.exe");
-//                if !clang_path.exists() || !clang_path.is_file() {
-//                    match which::which("clang-cl") {
-//                        Ok(path) => clang_path = path,
-//                        Err(..) => panic!("Rex requires clang-cl on Windows. Please add it to path, set LLVM_DIR to
-// the root of your LLVM install or install LLVM to C:/Program Files/LLVM/"),                    }
-//                }
-//                self.compiler(clang_path)
-//            } else {
-//                self
-//            }
-//        }
-//    }
-//    fn add_defines(&mut self) -> &mut Self {
-//        cfg_if::cfg_if! {
-//            if #[cfg(target_os = "windows")] {
-//                self.define("_CRT_SECURE_NO_WARNINGS", None)
-//            } else {
-//                self.define("_DEFAULT_SOURCE", None)
-//            }
-//        }
-//    }
-//    fn enable_c11(&mut self) -> &mut Self {
-//        cfg_if::cfg_if! {
-//            if #[cfg(target_os = "windows")] {
-//                self
-//            } else {
-//                self.flag_if_supported("-std=c11")
-//            }
-//        }
-//    }
-//    fn enable_cpp17(&mut self) -> &mut Self {
-//        cfg_if::cfg_if! {
-//            if #[cfg(target_os = "windows")] {
-//                self.flag_if_supported("/std:c++17")
-//            } else {
-//                self.flag_if_supported("-std=c++17")
-//            }
-//        }
-//    }
-//}
+impl BuildExt for cmake::Config {
+    fn enable_clang_cl(&mut self) -> &mut Self {
+        cfg_if::cfg_if! {
+            if #[cfg(target_os = "windows")] {
+                let llvm_dir = std::env::var("LLVM_DIR").ok();
+                let llvm_dir = llvm_dir.unwrap_or_else(|| String::from("C:\\Program Files\\LLVM\\"));
+                let mut clang_path = PathBuf::from(llvm_dir);
+                clang_path.push("bin");
+                clang_path.push("clang-cl.exe");
+                if !clang_path.exists() || !clang_path.is_file() {
+                    match which::which("clang-cl") {
+                        Ok(path) => clang_path = path,
+                        Err(..) => panic!("Rex requires clang-cl on Windows. Please add it to path, set LLVM_DIR to
+        the root of your LLVM install or install LLVM to C:/Program Files/LLVM/"),                    }
+                }
+                // Cause cmake is fuckin stupid
+                let fixed_path = clang_path.display().to_string().replace("\\", "/");
+                self.define("CMAKE_C_COMPILER", &fixed_path).define("CMAKE_CXX_COMPILER", fixed_path).generator("Ninja")
+            } else {
+                self
+            }
+        }
+    }
+}
 
 #[derive(Eq, PartialEq, Debug, Clone)]
 enum FileTypes {
@@ -131,16 +100,20 @@ fn main() {
 
     run_bindgen();
 
-    let location = cmake::Config::new(".").build();
+    let location = cmake::Config::new(".")
+        .enable_clang_cl()
+        .define("SDL2_INCLUDE_DIR", std::env::var("DEP_SDL2_INCLUDE").unwrap())
+        .define("SDL2_LINK_SEARCH", std::env::var("DEP_SDL2_LINK_SEARCH").unwrap())
+        .build();
 
     println!("cargo:rustc-link-search=native={}", location.display());
     println!("cargo:rustc-link-lib=dylib=bverex");
-    println!("cargo:rustc-link-lib=dylib=SDL2");
 }
 
 fn run_bindgen() {
     let bindings = bindgen::builder()
         .clang_arg("-Irex/src")
+        .clang_arg("-std=c++17")
         .header("wrapper/wrapper.hpp")
         .whitelist_type("bve::.*")
         .whitelist_type("rx::game$")
