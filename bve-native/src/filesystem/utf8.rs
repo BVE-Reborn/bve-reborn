1385973b35ecd1266f46c0985eeee83473b3dd06
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-02-12 16:44:03 -0500

    [native] Fix variable

--- bve-native/src/filesystem/utf8.rs
+++ bve-native/src/filesystem/utf8.rs
@@ -13,10 +13,10 @@ use std::ptr::NonNull;
 /// - If file loading fails, output is null.
 #[must_use]
 #[c_interface]
-pub unsafe extern "C" fn bve_filesystem_read_convert_utf8(filename: *const c_char) -> Option<NonNull<c_char>> {
-    let cow = unowned_ptr_to_str(&filename);
+pub unsafe extern "C" fn bve_filesystem_read_convert_utf8(filename_ptr: *const c_char) -> Option<NonNull<c_char>> {
+    let filename = unowned_ptr_to_str(&filename_ptr);
 
-    let result = bve::filesystem::read_convert_utf8(cow.as_ref());
+    let result = bve::filesystem::read_convert_utf8(filename.as_ref());
 
     result
         .ok()
