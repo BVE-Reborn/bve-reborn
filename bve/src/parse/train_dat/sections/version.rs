f75c99333ca6e55372fe3dc850993c95c830300b
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-02-27 15:01:09 -0500

    [parse::train_dat] Clean up control flow

--- bve/src/parse/train_dat/sections/version.rs
+++ bve/src/parse/train_dat/sections/version.rs
@@ -24,20 +24,15 @@ impl Default for Version {
 
 impl FromKVPValue for Version {
     fn from_kvp_value(value: &str) -> Option<Self> {
-        match value {
-            "bve1200000" => Some(Self::BVE120),
-            "bve1210000" => Some(Self::BVE121),
-            "bve1220000" => Some(Self::BVE122),
-            "bve2000000" | "openbve" => Some(Self::BVE2),
-            _ => {
-                if value.starts_with("openbve") {
-                    Some(Self::OpenBVE {
-                        version: String::from(&value[7..]),
-                    })
-                } else {
-                    None
-                }
-            }
-        }
+        Some(match value {
+            "bve1200000" => Self::BVE120,
+            "bve1210000" => Self::BVE121,
+            "bve1220000" => Self::BVE122,
+            "bve2000000" | "openbve" => Self::BVE2,
+            _ if value.starts_with("openbve") => Self::OpenBVE {
+                version: String::from(&value[7..]),
+            },
+            _ => return None,
+        })
     }
 }
