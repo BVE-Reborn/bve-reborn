e9cb98eda09e1c6c7c4f798ef3f0d0cd200cbbb9
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-02-27 20:28:27 -0500

    [parse::train_dat] Add train.dat parsing function

--- bve/src/parse/train_dat/mod.rs
+++ bve/src/parse/train_dat/mod.rs
@@ -1,3 +1,13 @@
+use crate::parse::kvp::{parse_kvp_file, FromKVPFile, KVPGenericWarning, DAT_LIKE};
+use crate::parse::util::strip_comments;
 pub use sections::*;
 
 mod sections;
+
+#[must_use]
+pub fn parse_train_dat(input: &str) -> (ParsedTrainDat, Vec<KVPGenericWarning>) {
+    let lower = strip_comments(input, ';').to_lowercase();
+    let kvp_file = parse_kvp_file(&lower, DAT_LIKE);
+
+    ParsedTrainDat::from_kvp_file(&kvp_file)
+}
