4b2d532ff898d6eb0563caedc0600a0579972284
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-05 20:08:10 -0500

    [parse::extensions_cfg] Finish sections

--- bve/src/parse/extensions_cfg/sections/mod.rs
+++ bve/src/parse/extensions_cfg/sections/mod.rs
@@ -2,9 +2,15 @@ use bve_derive::FromKVPFile;
 use std::collections::HashMap;
 
 pub mod car;
+pub mod coupler;
+pub mod exterior;
 
 #[derive(Debug, Default, Clone, PartialEq, FromKVPFile)]
 pub struct ParsedExtensionsCfg {
     #[kvp(rename = "car")]
     pub cars: HashMap<u64, car::CarSection>,
+    #[kvp(rename = "coupler")]
+    pub couplers: HashMap<u64, coupler::CouplerSection>,
+    #[kvp(rename = "exterior")]
+    pub exteriors: HashMap<u64, exterior::ExteriorSection>,
 }
