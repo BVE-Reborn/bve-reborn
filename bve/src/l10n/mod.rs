6ce0fc8c624ad13a0db9d19eb1a402ce84221c15
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-08 17:24:29 -0400

    [l10n] Clean up macros and add english-only localization

--- bve/src/l10n/mod.rs
+++ bve/src/l10n/mod.rs
@@ -11,6 +11,7 @@ mod current;
 mod load;
 
 pub static CURRENT_BVE_LOCALE: Lazy<RwLock<BVELocale>> = Lazy::new(|| RwLock::new(load_locale(get_current_language())));
+pub static ENGLISH_LOCALE: Lazy<BVELocale> = Lazy::new(|| load_locale(Language::EN));
 
 pub struct BVELocale {
     pub language: Language,
@@ -44,19 +45,28 @@ impl Display for Language {
 
 #[macro_export]
 macro_rules! localize {
+    // Localize in english for logging
+    (@en $name:literal, $($key:literal -> $value:literal),+ $(,)*) => {
+        $crate::localize!($crate::l10n::ENGLISH_LOCALE, $name, $($key -> $value),+)
+    };
+    (@en $name:literal) => {
+        $crate::localize!($crate::l10n::ENGLISH_LOCALE, $name)
+    };
+    // Localize in the current locale
     ($name:literal, $($key:literal -> $value:literal),+ $(,)*) => {
         $crate::localize!($crate::l10n::CURRENT_BVE_LOCALE.read().expect("Unable to lock LocaleMutex"), $name, $($key -> $value),+)
     };
     ($name:literal) => {
         $crate::localize!($crate::l10n::CURRENT_BVE_LOCALE.read().expect("Unable to lock LocaleMutex"), $name)
     };
+    // Localize over the locale provided as first argument, taken by reference.
     ($locale:expr, $name:literal, $($key:literal -> $value:literal),+ $(,)*) => {{
         let mut errors = std::vec::Vec::new();
         let mut args = std::collections::HashMap::new();
         $(
             args.insert($key, fluent::FluentValue::from($value));
         )*
-        let guard = $locale;
+        let guard = &$locale;
         let msg = guard.bundle.get_message($name).expect("Missing translation name");
         let pattern = msg.value.expect("Message has no pattern");
         let formatted: std::string::String = guard.bundle.format_pattern(&pattern, Some(&args), &mut errors).to_string();
@@ -65,7 +75,7 @@ macro_rules! localize {
     }};
     ($locale:expr, $name:literal) => {{
         let mut errors = std::vec::Vec::new();
-        let guard = $locale;
+        let guard = &$locale;
         let msg = guard.bundle.get_message($name).expect("Missing translation name");
         let pattern = msg.value.expect("Message has no pattern");
         let formatted: std::string::String = guard.bundle.format_pattern(&pattern, None, &mut errors).to_string();
@@ -89,8 +99,7 @@ mod test {
         ($name:ident, $lang:ident) => {
             #[test]
             fn $name() {
-                let language_value = load_locale(Language::$lang);
-                let language = &language_value;
+                let language = load_locale(Language::$lang);
                 loc_test!(language, "name");
                 loc_test!(language, "language-code");
                 loc_test!(language, "welcome", "name" -> "MyUsername");
