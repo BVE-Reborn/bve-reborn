6b5e66833fc664697b4b1fbc1254911ea5777a3a
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-16 02:16:50 -0400

    [parse::panel1] Add high level parse function

--- bve/src/parse/panel1_cfg/mod.rs
+++ bve/src/parse/panel1_cfg/mod.rs
@@ -1,3 +1,13 @@
+use crate::parse::kvp::{parse_kvp_file, FromKVPFile, KVPGenericWarning, ANIMATED_LIKE};
+use crate::parse::util::strip_comments;
 pub use sections::*;
 
 mod sections;
+
+#[must_use]
+pub fn parse_panel1_cfg(input: &str) -> (ParsedPanel1Cfg, Vec<KVPGenericWarning>) {
+    let lower = strip_comments(input, ';').to_lowercase();
+    let kvp_file = parse_kvp_file(&lower, ANIMATED_LIKE);
+
+    ParsedPanel1Cfg::from_kvp_file(&kvp_file)
+}
