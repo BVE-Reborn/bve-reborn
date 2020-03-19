2dd4b55c7a09d0095f79d42770df58a79f769f9f
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-18 23:13:08 -0400

    [parse] Add a couple manual PrettyPrintResult impls

--- bve/src/parse/function_scripts/mod.rs
+++ bve/src/parse/function_scripts/mod.rs
@@ -2,8 +2,10 @@ mod ir;
 mod parser;
 
 use crate::parse::kvp::FromKVPValue;
+use crate::parse::PrettyPrintResult;
 pub use ir::*;
 pub use parser::*;
+use std::io;
 
 #[derive(Debug, Default, Clone, PartialEq)]
 pub struct ParsedFunctionScript {
@@ -19,5 +21,43 @@ impl From<Vec<Instruction>> for ParsedFunctionScript {
 impl FromKVPValue for ParsedFunctionScript {
     fn from_kvp_value(value: &str) -> Option<Self> {
         parse_function_script(value).map(|(_, o)| o).ok()
+    }
+}
+
+impl PrettyPrintResult for ParsedFunctionScript {
+    fn fmt(&self, indent: usize, out: &mut dyn io::Write) -> io::Result<()> {
+        out.write_all("\n".as_bytes())?;
+        for (idx, instruction) in self.instructions.iter().enumerate() {
+            out.write_all(&vec![b' '; indent * 4])?;
+            out.write_all(format!("{} - ", idx).as_bytes())?;
+            out.write_all(
+                match instruction {
+                    Instruction::Addition => "Addition",
+                    Instruction::Subtraction => "Subtraction",
+                    Instruction::Multiplication => "Multiplication",
+                    Instruction::Division => "Division",
+                    Instruction::LogicalOr => "LogicalOr",
+                    Instruction::LogicalAnd => "LogicalAnd",
+                    Instruction::LogicalXor => "LogicalXor",
+                    Instruction::UnaryLogicalNot => "UnaryLogicalNot",
+                    Instruction::UnaryNegative => "UnaryNegative",
+                    Instruction::Equal => "Equals",
+                    Instruction::NotEqual => "NotEquals",
+                    Instruction::Less => "Less",
+                    Instruction::Greater => "Greater",
+                    Instruction::LessEqual => "LessEqual",
+                    Instruction::GreaterEqual => "GreaterEqual",
+                    Instruction::FunctionCall { name, arg_count } => {
+                        format!("{}({} arguments)", name, arg_count).as_str()
+                    }
+
+                    Instruction::Variable { name } => format!("Variable: {}", name).as_str(),
+                    Instruction::Number { value } => format!("{}", value),
+                }
+                .as_bytes(),
+            )?;
+            out.write_all("\n".as_bytes());
+        }
+        Ok(())
     }
 }
--- bve/src/parse/train_dat/sections/version.rs
+++ bve/src/parse/train_dat/sections/version.rs
@@ -1,5 +1,7 @@
 use crate::parse::kvp::FromKVPValue;
+use crate::parse::PrettyPrintResult;
 use bve_derive::FromKVPSection;
+use std::io;
 
 #[derive(Debug, Default, Clone, PartialEq, FromKVPSection)]
 pub struct VersionSection {
@@ -34,5 +36,21 @@ impl FromKVPValue for Version {
             },
             _ => return None,
         })
+    }
+}
+
+impl PrettyPrintResult for Version {
+    fn fmt(&self, indent: usize, out: &mut dyn io::Write) -> io::Result<()> {
+        out.write(
+            match self {
+                Version::BVE120 => "BVE1200000",
+                Version::BVE121 => "BVE1210000",
+                Version::BVE122 => "BVE1220000",
+                Version::BVE2 => "BVE2000000",
+                Version::OpenBVE { version } => version.as_str(),
+            }
+            .as_bytes(),
+        )?;
+        Ok(())
     }
 }
