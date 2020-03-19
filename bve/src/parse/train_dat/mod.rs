3ef2e1a288390772a60c7222923411cfdbd26552
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-18 23:11:58 -0400

    [parse::train_dat] try using new parser interface

--- bve/src/parse/train_dat/mod.rs
+++ bve/src/parse/train_dat/mod.rs
@@ -1,18 +1,9 @@
-use crate::parse::kvp::{parse_kvp_file, FromKVPFile, KVPGenericWarning, KVPSymbols, DAT_LIKE};
-use crate::parse::util::strip_comments;
+use crate::parse::kvp::{KVPSymbols, DAT_LIKE};
 use crate::parse::KVPFileParser;
 pub use sections::*;
 use std::fmt;
 
 mod sections;
-
-#[must_use]
-pub fn parse_train_dat(input: &str) -> (ParsedTrainDat, Vec<KVPGenericWarning>) {
-    let lower = strip_comments(input, ';').to_lowercase();
-    let kvp_file = parse_kvp_file(&lower, DAT_LIKE);
-
-    ParsedTrainDat::from_kvp_file(&kvp_file)
-}
 
 impl fmt::Display for ParsedTrainDat {
     fn fmt(&self, _: &mut fmt::Formatter<'_>) -> fmt::Result {
