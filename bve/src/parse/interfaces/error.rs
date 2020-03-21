29484d20f6b9eba3370aff8c1010cb7b4eef2c89
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-21 00:25:07 -0400

    [parse::interfaces] Add beginnings of error interface

--- bve/src/parse/interfaces/error.rs
+++ bve/src/parse/interfaces/error.rs
@@ -1,15 +1,48 @@
+use crate::l10n::ForceEnglish;
+
 // Temporary
 pub trait UserError {
-    fn print(&self) -> String
-    where
-        Self: ToString,
-    {
-        self.to_string()
+    fn to_data(&self) -> UserErrorData {
+        UserErrorData {
+            category: self.category(),
+            line: self.line(),
+            description: self.description(ForceEnglish::Local),
+            description_english: self.description(ForceEnglish::English),
+        }
     }
+
+    fn category(&self) -> UserErrorCategory;
+    fn line(&self) -> u64;
+
+    fn description(&self, en: ForceEnglish) -> String;
+}
+
+pub struct UserErrorData {
+    pub category: UserErrorCategory,
+    pub line: u64,
+    pub description: String,
+    pub description_english: String,
+}
+
+pub enum UserErrorCategory {
+    Warning,
+    Error,
 }
 
 impl UserError for () {
-    fn print(&self) -> String {
+    fn to_data(&self) -> UserErrorData {
+        unreachable!("Types that have () as their error/warning type should return only empty vectors of them.");
+    }
+
+    fn category(&self) -> UserErrorCategory {
+        unreachable!("Types that have () as their error/warning type should return only empty vectors of them.");
+    }
+
+    fn line(&self) -> u64 {
+        unreachable!("Types that have () as their error/warning type should return only empty vectors of them.");
+    }
+
+    fn description(&self, _en: ForceEnglish) -> String {
         unreachable!("Types that have () as their error/warning type should return only empty vectors of them.");
     }
 }
