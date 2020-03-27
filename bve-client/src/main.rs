80f5aa10c0047561632891b07b7b55280da2c918
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-03-27 18:17:51 -0400

    [client] Begin making a window

--- bve-client/src/main.rs
+++ bve-client/src/main.rs
@@ -1,1 +1,22 @@
-fn main() {}
+use winit::event_loop::EventLoop;
+use winit::window::WindowBuilder;
+
+async fn async_main() {
+    let event_loop = EventLoop::new();
+
+    env_logger::init();
+
+    let (window, size) = {
+        let mut builder = WindowBuilder::new();
+        builder = builder.with_title("BVE-Reborn");
+        let window = builder.build(&event_loop).unwrap();
+        let size = window.inner_size();
+        (window, size)
+    };
+
+
+}
+
+fn main() {
+    futures::executor::block_on(async_main())
+}
