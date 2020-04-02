75d2f9e2b8dbe23ae9b1ac0174af8fe7b7d43323
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-04-01 23:58:28 -0400

    [render] Do mesh color math in linear color

--- bve-render/shaders/test.fs.glsl
+++ bve-render/shaders/test.fs.glsl
@@ -2,7 +2,7 @@
 
 layout(location = 0) in vec2 texcoord;
 layout(location = 1) in vec3 normal;
-layout(location = 2) flat in vec4 mesh_color;
+layout(location = 2) flat in vec4 mesh_color_srgb;
 layout(location = 0) out vec4 outColor;
 
 layout(set = 0, binding = 0) uniform Uniforms {
@@ -14,6 +14,7 @@ layout(set = 0, binding = 2) uniform sampler main_sampler;
 
 void main() {
     vec4 tex_color = pow(vec4(texture(usampler2D(color, main_sampler), texcoord)) / 255, vec4(2.2));
+    vec4 mesh_color = pow(mesh_color_srgb, vec4(2.2));
     vec4 color = tex_color * mesh_color;
     if (!bool(uniforms.transparent) && color.a <= 0.5) {
         discard;
