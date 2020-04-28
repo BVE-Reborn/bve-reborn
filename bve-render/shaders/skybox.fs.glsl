7f4383dea09d9d1bbc6dcdb830fd0393e19057b4
Connor Fitzgerald <connorwadefitzgerald@gmail.com>
2020-04-27 23:27:03 -0400

    [render] Get yaw and pitch from position vector

--- bve-render/shaders/skybox.fs.glsl
+++ bve-render/shaders/skybox.fs.glsl
@@ -1,5 +1,7 @@
 #version 450
 
+#define PI 3.1415926535626433
+
 layout(location = 0) in vec2 clip_position;
 layout(location = 0) out vec4 outColor;
 
@@ -13,6 +15,13 @@ void main() {
     vec4 clip = vec4(clip_position, 1.0, 1.0);
     vec4 world = inv_view_proj * clip;
     world.xyz /= world.w;
+    vec3 world_dir = normalize(vec3(world));
 
-    outColor = vec4(pow(vec3(normalize(world)), vec3(1 / 2.2)), 1.0);
+    // both [0, tau]
+    // normally atan is [-pi, pi] around +z, but we want it [0, tau] around +z, so we need to flip Z and offset it by PI.
+    // The normal rotation direction is counter clockwise -pi -> pi, but we need clockwise, so flip the resulting sign to make it pi -> -pi.
+    float inv_yaw = -atan(world_dir.x, -world_dir.z) + PI;
+    float pitch = asin(world_dir.y) * 2 + PI;
+
+    outColor = vec4(vec2(yaw / (PI * 2), 0.0), 0.0, 1.0);
 }
